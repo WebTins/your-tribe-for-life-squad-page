d43fd42b846318aa4b9194258d8b54bf471dab3a
Tin Nguyen <powertinnguyen@gmail.com>
2026-09-02 23:34:52 +0200

    #9 Content first
    Added the content that is needed for the page. Tested and it's showing on the error page. Now only needs styling and the backbutton component added to it

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { page } from '$app/state';
+    let src = '/src/lib/assets/spilled-coffee.png';
+    let alt = 'spilled coffee'
+</script>
+
+<h1 class="error">{page.status}</h1>
+
+<img src= {src} width="200" alt= {alt}>
+
+<h2 class="error-message">ERROR: PAGE NOT FOUND</h2>
+
+<p>We couldn't find the page you were looking for... maybe it's time for a coffee break?</p>
