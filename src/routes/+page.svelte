0cc8b5e5f4927fa53cb658931d2687c41abee690
Isaac Eswa <isaaceswa@gmail.com>
2026-09-03 16:09:43 +0200

    #12 #8 name, residency and mugshot are dynamic
    Used <picture> for PE and performance

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,35 @@
   const persons = data.persons;
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
-</p>
-
 {#each persons as person}
-  {person.name}<br />
+  <article class="person-card">
+    <h2>{person.name}</h2>
+    <p>{person.residency}</p>
+
+    {#if person.mugshot}
+      <picture>
+        <source
+          type="image/avif"
+          srcset="https://fdnd.directus.app/assets/{person.mugshot}?fit=cover&format=avif&height=700&quality=80"
+        />
+
+        <source
+          type="image/webp"
+          srcset="https://fdnd.directus.app/assets/{person.mugshot}?fit=cover&format=webp&height=700&quality=80"
+        />
+
+        <img
+          src="https://fdnd.directus.app/assets/{person.mugshot}?height=700&quality=80"
+          width={person.mugshot.width}
+          height={person.mugshot.height}
+          alt="Afbeelding van {person.name}"
+        />
+      </picture>
+    {:else}
+      <img
+        src="/assets/person-placeholder.png"
+        alt="Geen afbeelding beschikbaar"
+      />
+    {/if}
+  </article>
 {/each}
