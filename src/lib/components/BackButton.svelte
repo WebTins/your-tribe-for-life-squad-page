9d947915cbc64041bc127637338c483da6e0a8ba
Tin Nguyen <powertinnguyen@gmail.com>
2026-09-03 22:06:47 +0200

    #10 Added styling to the button
    Added the styling that was needed for the button as in de Design with the mask it should have. Made it a component that can be reused in other pages as well.

--- src/lib/components/BackButton.svelte
+++ src/lib/components/BackButton.svelte
@@ -6,3 +6,25 @@
     Back to home
 </button>
 
+<style>
+    .back-button {
+        background: #000000;
+        color: white;
+        border: none;
+        width: 200px;
+        height: 150px;
+        cursor: pointer;
+
+        padding: 1rem 2rem;
+
+        clip-path: polygon(
+            13% 30%,
+            58% 31%,
+            97% 30%,
+            91% 60%,
+            35% 61%,
+            16% 64%,
+            0% 50%
+        );
+    }
+</style>
