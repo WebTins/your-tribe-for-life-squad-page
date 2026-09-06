19c4c4a68b440bde00b95d4617e6d7fa75ad60bb
JasinAhmed <jaasin9@gmail.com>
2026-09-06 15:13:30 +0200

    #16 #11  refactor: move countdown styles to parent

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,28 @@
 <script>
-  let { data } = $props();
+	import CountdownClock from '$lib/components/CountdownClock.svelte'
 
-  const persons = data.persons;
+	let { data } = $props()
+
+	const persons = data.persons
 </script>
 
+<CountdownClock />
+
 <h1>Welcome to SvelteKit</h1>
+
 <p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+	documentation
 </p>
 
 {#each persons as person}
-  {person.name}<br />
+	{person.name}<br />
 {/each}
+
+<style>
+	:global(:root) {
+		--countdown-background: #000000;
+		--countdown-text: #ffffff;
+		--countdown-width: 400px;
+	}
+</style>
