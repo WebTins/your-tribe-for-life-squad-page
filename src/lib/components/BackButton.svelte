b9a17a1b8bb26eb7c0e8eabf2281809e334ca774
Tin Nguyen <powertinnguyen@gmail.com>
2026-09-06 18:33:08 +0200

    #10 HTML Fix
    Changed the button to a link and added SVG for the arrow.

--- src/lib/components/BackButton.svelte
+++ src/lib/components/BackButton.svelte
@@ -1,10 +1,11 @@
-<script>
-    import { goto } from '$app/navigation'
-</script>
-
-<button class="back-button" onclick={() => goto('/')}>
-    Back to home
-</button>
+<a href="/" class="back-button">
+    <span>
+    <svg width="25" viewBox="0 0 54 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0.878681 19.9705C-0.292892 21.142 -0.292892 23.0415 0.878681 24.2131L19.9706 43.305C21.1421 44.4766 23.0416 44.4766 24.2132 43.305C25.3848 42.1334 25.3848 40.2339 24.2132 39.0624L7.24264 22.0918L24.2132 5.12123C25.3848 3.94966 25.3848 2.05017 24.2132 0.878593C23.0416 -0.29298 21.1421 -0.29298 19.9706 0.878593L0.878681 19.9705ZM53.5 22.0918V19.0918L3 19.0918V22.0918V25.0918L53.5 25.0918V22.0918Z" fill="white"/>
+    </svg>
+        Back to home
+    </span> 
+</a>
 
 <style>
     .back-button {
