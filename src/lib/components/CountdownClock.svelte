8fb0608458e72a81ad8127d37de74d697e475116
Tin Nguyen <powertinnguyen@gmail.com>
2026-09-17 14:01:32 +0200

    #38 Added props
    Added props that can be reused on other files for the components

--- src/lib/components/CountdownClock.svelte
+++ src/lib/components/CountdownClock.svelte
@@ -5,6 +5,7 @@
 	let hours = $state(0)
 	let minutes = $state(0)
 	let seconds = $state(0)
+	let { transitionName = 'countdown-clock' } = $props()
 
 	function updateCountdown() {
 		const now = new Date()
@@ -37,7 +38,7 @@
 	})
 </script>
 
-<div class="countdown-wrapper">
+<div class="countdown-wrapper" style="view-transition-name: {transitionName};">
 	<header class="countdown-clock">
 		<a href="/" class="squad-info">
 			<img src={logo} alt="2G Squadpage logo" />
